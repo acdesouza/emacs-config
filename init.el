e586c1cbc941a775b83f8abaf5875a3448b0acfc
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2021-01-11 11:26:50 -0300

    Initial customization. - No menu. More lines visible - No scrollbar. So, I can vertically resize using the mouse in the whole   line - Show line numbers - Change default font. And use a color theme closer to the one I use in Vim - Remove unecessary whitespaces in lines and empty lines in the end of   the file - Use the package manager to install things - Add config to allow download from package repo MELPA in MacOS

---
+++ init.el
@@ -0,0 +1,39 @@
+(setq inhibit-startup-message t)
+
+(menu-bar-mode -1)	;; Removes menu
+(tool-bar-mode -1)	;; Removes toolbar
+(scroll-bar-mode -1)	;; Removes scrollbar
+(global-linum-mode 1)	;; Add line number
+
+(add-to-list 'before-save-hook 'delete-trailing-whitespace) ;; Remove whitespaces before save file
+
+
+(define-key global-map (kbd "RET") 'newline-and-indent) ;; Auto-indent
+
+(set-face-attribute 'default nil :font "Menlo-16")
+
+;; Initialize package sources
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package gruvbox-theme
+  :ensure t
+  :init
+  (load-theme 'gruvbox-dark-medium t)
+  :config
+  (show-paren-mode t)
+  (setq show-paren-style 'expression))
