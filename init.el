fe247ba22a0ad2981f4dae04d433262c455a9d24
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2021-07-25 17:06:05 -0300

    [Projectile] Sets frame name() to project's name, on project change.

--- init.el
+++ init.el
@@ -124,8 +124,24 @@
               ("s-p" . projectile-command-map)
               ("C-c p" . projectile-command-map))
   :config (progn
-	    (setq projectile-switch-project-action #'projectile-dired)
-
+            (setq projectile-switch-project-action #'projectile-dired)
+            ;; https://tecosaur.github.io/emacs-config/config.html#window-title
+            (defun change-frame-name-on-project-change ()
+              (setq frame-title-format
+                    '(""
+                      (:eval
+                        (if (s-contains-p org-roam-directory (or buffer-file-name ""))
+                          (replace-regexp-in-string
+                            ".*/[0-9]*-?" "☰ "
+                            (subst-char-in-string ?_ ?  buffer-file-name))
+                          "%b"))
+                      (:eval
+                        (let ((project-name (projectile-project-name)))
+                          (unless (string= "-" project-name)
+                            (format (if (buffer-modified-p)  " ◉ %s" "  ●  %s") project-name))))))
+              ))
+  :hook
+  ((projectile-after-switch-project . change-frame-name-on-project-change)))
 
 
 ;; IDO
